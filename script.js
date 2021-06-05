2f8c36301751ac801c2a6e509c6968ff679fe5c6
Ложкин Егор Денисович <pl8389@pl1.edu>
2021-06-05 11:22:14 +0500

    hubgkuyg

--- script.js
+++ script.js
@@ -1,41 +1,175 @@
-let countcubes = 2 * 2;
-let arraycubes = [];
-for(let i = 0; i < countcubes / 2; i++){
-    let cube = {
-        red: Math.round(Math.random() * 255),
-        green: Math.round(Math.random() * 255),
-        blue: Math.round(Math.random() * 255),
-        view: false
-    }
-    arraycubes.push(cube);   
-}
-arraycubes = [...arraycubes, ...arraycubes];
-console.log(arraycubes);
-function renderCubes(arraycubes){
-    arraycubes.map((cube, index) => {
-        if(document.getElementById('cube_' + index)){
-        let div = document.createElement('div');
-        div.id = 'cube_' + index;
-        div.className = 'cube';
-        div.onclick = event => {cubeClick(event);}
-        document.getElementById('cubes').append(div);
+let countKubiks = 4 * 4;
+let arrayKubiks = [];
+let arrayColors = [];
+
+let firstKubik = null;
+let countFailed = 0;
+
+let isEnd = true;
+
+document.getElementById('kubiks').style.width = 4 * 100 + 22 * 4 + 'px';
+
+
+
+function randomKubiks() {
+    for (let i = 0; i < countKubiks / 2; i++) {
+        let color = {
+            red: Math.round(Math.random() * 255),
+            green: Math.round(Math.random() * 255),
+            blue: Math.round(Math.random() * 255)
         }
-        rebderCube(cube);
+    
+        arrayColors.push(color);
+        arrayColors.push(color);
+    }
+
+    arrayColors = arrayColors.sort(function() {
+        return Math.random() - 0.5;
+    });
+
+    for (let i = 0; i < arrayColors.length; i++) {
+        let kubik = {
+            id: i,
+            red: arrayColors[i].red,
+            green: arrayColors[i].green,
+            blue: arrayColors[i].blue,
+            view: false
+        }
+    
+        arrayKubiks.push(kubik);
+    }
+
+}
+
+randomKubiks();
+
+console.log(arrayKubiks);
+
+function renderKubiks(arrayKubiks) {
+    arrayKubiks.map(kubik => {
+        if (!document.getElementById('kubik_' + kubik.id)) {
+            let div = document.createElement('div');
+            div.id = 'kubik_' + kubik.id;
+            div.className = 'kubik';
+            div.onclick = event => kubikClick(event);
+            document.getElementById('kubiks').append(div);
+        }
+
+        renderKubik(kubik);
     });
 }
-function renderCube(cube, index){
-    if(cube.view){
-        document.getElementById('cube_' + index).style.backgroundColor = 
-        'rgb(' + cube.red + ',' +cube.green + ',' + cube.blue + ')';
+
+
+function renderKubik(kubik) {
+    if (kubik.view) {
+        document.getElementById('kubik_' + kubik.id).style.
+        backgroundColor = 
+        'rgb(' + kubik.red + ',' + kubik.green + ',' + kubik.blue + ')';
+    }
+
+    else {
+        document.getElementById('kubik_' + kubik.id).style.
+        backgroundColor = 'rgb(0,0,0)';
     }
 }
-renderCubes(arraycubes);
-function cubeClick(event){
-    let cubeId = Number(event.target.id.slice(6));
-    arraycubes.map((cube, index) =>{
-        if (index = cubeId){
-            cube.view = true;
+
+
+renderKubiks(arrayKubiks);
+
+
+
+function kubikClick(event) {
+    let kubikId = Number(event.target.id.slice(6));
+
+    let isRollback = false;
+    let isForward = false;
+
+
+    arrayKubiks.map(kubik => {
+        if (kubik.id === kubikId) {
+
+            if (firstKubik) {
+                if (firstKubik.red === kubik.red &&
+                    firstKubik.green === kubik.green &&
+                    firstKubik.blue === kubik.blue) {
+                        isForward = true;
+                    }
+                    else {
+                        isRollback = true;
+                    }
+            }
+
+            else {
+                firstKubik = kubik;
+            }
+
+
+
+            kubik.view = true;
         }
     });
-    renderCubes(arrayCubes);
+
+    renderKubiks(arrayKubiks);
+
+
+    if (isRollback) {
+        const firstId = firstKubik.id;
+
+        sleep(500).then(() => {
+            arrayKubiks.map(kubik => {
+                if ((kubik.id === kubikId) ||
+                 (kubik.id === firstId)) {
+                    kubik.view = false;
+                }
+            });
+
+            renderKubiks(arrayKubiks);
+        });
+
+        firstKubik = null;
+        
+        countFailed += 1;
+        console.log('Вы совершили ' + countFailed + ' неудачных попыток.');
+    }
+
+
+    if (isForward) {
+        isEnd = true;
+
+        arrayKubiks.map(kubik => {
+            if (!kubik.view) {
+                isEnd = false;
+            }
+        });
+
+        if (isEnd) {
+            console.log('Игра окончена с ' + countFailed + ' неудач');
+            
+            const result = confirm('дальше');
+            if(result){
+                arrayColors = [];
+                arrayKubiks = [];
+
+                firstKubik = null;
+                countFailed =0;
+
+                isEnd = true;
+                randomKubiks();
+                renderKubik(arrayKubiks);
+            }
+
+        }
+        
+        else {
+            console.log('продолжай');
+        }
+
+        firstKubik = null;
+
+        renderKubiks(arrayKubiks);
+    }
+}
+
+function sleep(ms) {
+    return new Promise(func => setTimeout(func, ms));
 }
